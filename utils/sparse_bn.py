2e07716e7fbdb887a165b95d828017eaef84df8c
SunJianBo <1871593109@qq.com>
2022-12-30 03:33:55 +0000

    sparse bn.

--- utils/sparse_bn.py
+++ utils/sparse_bn.py
@@ -21,12 +21,24 @@ class OptimizerBN:
         self.model = model                                      # 网络模型
         self.sr = sr                                            # 稀疏化系数,也就是L1正则化的系数
         self.epochs = epochs                                    # 用于动态改变权重系数
-        self.ignore_bn_list = []                                # 不需要进行稀疏训练的层,通常是bottleneck层
+        self.ignore_bn_list = self.acquire_ignore_bn_list()     # 不需要进行稀疏训练的层,通常是bottleneck层
+        
         
     def update_bn(self, epoch):
         # sr被称为稀疏率(sparsity rate),我觉得就是L1正则化系数
         srtmp = self.sr * (1 - 0.9 * epoch / self.epochs)                         # BN.weight的系数
         
+        for k, m in self.model.named_modules():
+            if isinstance(m, nn.BatchNorm2d) and (k not in self.ignore_bn_list):
+                # 为什么对BN参数使用L1正则？因为它可以稀疏化参数,使参数趋于零
+                # L1损失函数是绝对值,它的倒数是-1、0、1
+                # 因此每一个参数加入L1正则之后的梯度是在原有梯度基础上加上一个数值(sr * sign(BN.weight.data))
+                m.weight.grad.data.add_(srtmp * torch.sign(m.weight.data))                      # L1 正则,直接处理梯度      
+                m.bias.grad.data.add_(self.sr * 10 * torch.sign(m.bias.data))                        # L1 正则,这个可以不需要
+
+
+    def acquire_ignore_bn_list(self):
+        ignore_bn_list = []
         for k, m in self.model.named_modules():
             if isinstance(m, Bottleneck):
                 # 存在瓶颈层的这一块是不剪枝的
@@ -34,17 +46,17 @@ class OptimizerBN:
                 # 这里是针对C3层的处理策略,其它网络类似
                 # 这里所说的C3是带跳跃链接的C3
                 if m.add:                                                           # 存在跳跃链接
-                    self.ignore_bn_list.append(k.rsplit(".", 2)[0] + "cv1.bn")              # C3网络层中的cv1卷积的BN不正则化
-                    self.ignore_bn_list.append(k + ".cv1.bn")                            # BottleNect中的cv1卷积的BN
-                    self.ignore_bn_list.append(k + ".cv2.bn")                            # BottleNect中的cv2卷积的BN
-            if isinstance(m, nn.BatchNorm2d) and (k not in self.ignore_bn_list):
+                    ignore_bn_list.append(k.rsplit(".", 2)[0] + "cv1.bn")              # C3网络层中的cv1卷积的BN不正则化
+                    ignore_bn_list.append(k + ".cv1.bn")                            # BottleNect中的cv1卷积的BN
+                    ignore_bn_list.append(k + ".cv2.bn")                            # BottleNect中的cv2卷积的BN
+            # if isinstance(m, nn.BatchNorm2d) and (k not in self.ignore_bn_list):
                 
-                # 为什么对BN参数使用L1正则？因为它可以稀疏化参数,使参数趋于零
-                # L1损失函数是绝对值,它的倒数是-1、0、1
-                # 因此每一个参数加入L1正则之后的梯度是在原有梯度基础上加上一个数值(sr * sign(BN.weight.data))
-                m.weight.grad.data.add_(srtmp * torch.sign(m.weight.data))                      # L1 正则,直接处理梯度      
-                m.bias.grad.data.add_(self.sr * 10 * torch.sign(m.bias.data))                        # L1 正则,这个可以不需要
-
+            #     # 为什么对BN参数使用L1正则？因为它可以稀疏化参数,使参数趋于零
+            #     # L1损失函数是绝对值,它的倒数是-1、0、1
+            #     # 因此每一个参数加入L1正则之后的梯度是在原有梯度基础上加上一个数值(sr * sign(BN.weight.data))
+            #     m.weight.grad.data.add_(srtmp * torch.sign(m.weight.data))                      # L1 正则,直接处理梯度      
+            #     m.bias.grad.data.add_(self.sr * 10 * torch.sign(m.bias.data))                        # L1 正则,这个可以不需要
+        return ignore_bn_list
 
     def display_bn(self):
         module_weight_list = []                                                                 # 存储每一个BN的权重值,也就是gamma的值
